380c1e6aba43e5177a44499a6303bf37d9966d1f
James Fairbanks <james.fairbanks@gatech.edu>
2013-03-18 19:02:40 -0400

    pull the parameters from the filename

--- plotter.py
+++ plotter.py
@@ -1,26 +1,39 @@
 from __future__ import print_function
+import os.path as path
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import itertools
 
-def unpack(ntupl):
-    return (ntupl[0][0], ntupl[0][1], ntupl[1])
+def tuple_repr(t):
+    """Format a tuple to go into a filename with periods instead of commas
+       Ends in a period.
+    :t: the tuple to take
+    :returns: item0.item1...
+
+    """
+    perf = lambda i: str(i)+'.'
+    string = ''.join(map(perf, t))
+    return string
 
 prots = ['S', 'K', 'L', 'V']
 media = ['elastin', 'gelatin']
 
-titlefmt    = 'cat%s eats cat%s on %s'
-filenamefmt = 'data.%s.%s.%s.csv'
-trials = itertools.product(itertools.permutations(prots[:2]), media)
-for ntupl in trials:
-    print(ntupl)
-    tupl = unpack(ntupl)
+length = 120
+titlefmt    = 'cat%s on %s'
+filenamefmt = 'data.%scsv'
+files = ['data.K.S.elastin.csv']
+for fname in files:
+    print(fname)
+    bname = path.basename(fname)
+    components = bname.split('.')
+    #if components[-1] is not 'csv':
+    #throw away the prefix and the fileextension
+    tupl = components[1:-1]
     print(tupl)
     #---- Load up some data ----#
-    filename = filenamefmt % tupl
-    title = titlefmt % tupl
-    df = pd.read_csv(filename)
+    title = titlefmt % (str(tupl[:-1]), tuple[-1])
+    df = pd.read_csv(fname)
     data_frame = df[['timevals', 'data']].set_index('timevals')
     model_frame = df[['time (min)', 'model']].set_index('time (min)')
 
@@ -29,7 +42,7 @@ for ntupl in trials:
     joined.plot(style={'data':'k+', 'model':'-'}, title=title)
     plt.xlabel('time (minutes)')
     plt.ylabel('concentartion (M)')
-    plt.savefig('plot.model.%s.%s.%s.png' % tupl)
+    plt.savefig('plot.model.%spng' % tuple_repr(tupl))
 
     #----- Computing the residuals from the model to the data ----#
     # we need to fix the fact that experiment and model do not 
@@ -40,9 +53,9 @@ for ntupl in trials:
     valid_indices = joined['data'].dropna().index
     resid = pd.Series(diffs.ix[valid_indices]['model'], name='residuals')
     resid.plot(title=title)
-    plt.plot(np.arange(120), np.zeros(120), 'k-', )
+    plt.plot(np.arange(length), np.zeros(length), 'k-', )
     plt.legend(); 
     plt.xlabel('time (minutes)')
     plt.ylabel('concentartion (M)')
-    plt.savefig('plot.resid.%s.%s.%s.png' % tupl)
+    plt.savefig('plot.resid.%spng' % tuple_repr(tupl))
     plt.show()
