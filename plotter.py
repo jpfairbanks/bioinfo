33d2d719d4b53f691f0597379df8be97f5f221bb
James <jpfairbanks@gmail.com>
2013-03-18 22:51:20 -0400

    refactor main to a function; add argparse;

--- plotter.py
+++ plotter.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import itertools
+import argparse
 
 def tuple_repr(t):
     """Format a tuple to go into a filename with periods instead of commas
@@ -16,30 +17,35 @@ def tuple_repr(t):
     string = ''.join(map(perf, t))
     return string
 
-prots = ['S', 'K', 'L', 'V']
-media = ['elastin', 'gelatin']
-
-length = 120
-titlefmt    = 'cat%s on %s'
-filenamefmt = 'data.%scsv'
-files = ['data.K.S.elastin.csv']
-for fname in files:
-    print(fname)
+def process_file(fname):
+    """Take a filename and make the plot and residual plot"""
+    if args.verbose:
+        print(fname)
     bname = path.basename(fname)
     components = bname.split('.')
     #if components[-1] is not 'csv':
     #throw away the prefix and the fileextension
     tupl = components[1:-1]
-    print(tupl)
+    if args.verbose:
+        print(tupl)
     #---- Load up some data ----#
     title = titlefmt % (str(tupl[:-1]), tupl[-1])
     df = pd.read_csv(fname)
-    data_frame = df[['timevals', 'data']].set_index('timevals')
-    model_frame = df[['time (min)', 'model']].set_index('time (min)')
+    data_col = df.columns[1]
+    if args.verbose:
+        print(df)
+	print(df.head())
+    data_frame = df[df.columns[0:2]].set_index('timevals')
+    model_frame = df[df.columns[4:6]].set_index('time (min)')
+    if args.verbose:
+	print(data_frame.head(10))
+	print(model_frame.head(10))
 
     # ---- Show how the model compares to the actual data ----#
     joined = data_frame.join(model_frame, how='outer')
-    joined.plot(style={'data':'ko', 'model':'-'}, title=title)
+    if args.verbose:
+	print(joined.head(10))
+    joined.plot(style={data_col:'ko', 'model':'-'}, title=title)
     plt.xlabel('time (minutes)')
     plt.ylabel('concentartion (M)')
     plt.savefig('plot.model.%spng' % tuple_repr(tupl))
@@ -48,14 +54,36 @@ for fname in files:
     # we need to fix the fact that experiment and model do not 
     # have the same index. So we fill and then subtract, then discard the
     # filled in values.
-    plt.figure()
-    diffs = joined.fillna().T.diff().T
-    valid_indices = joined['data'].dropna().index
-    resid = pd.Series(diffs.ix[valid_indices]['model'], name='residuals')
-    resid.plot(kind='bar', title=title)
-    plt.plot(np.arange(length), np.zeros(length), 'k-', )
-    plt.legend(); 
-    plt.xlabel('time (minutes)')
-    plt.ylabel('concentartion (M)')
-    plt.savefig('plot.resid.%spng' % tuple_repr(tupl))
-    plt.show()
+    if args.residuals:
+        plt.figure()
+        diffs = joined.fillna().T.diff().T
+        valid_indices = joined[data_col].dropna().index
+        resid = pd.Series(diffs.ix[valid_indices]['model'], name='residuals')
+        resid.plot(kind='bar', title=title)
+        plt.plot(np.arange(length), np.zeros(length), 'k-', )
+        plt.legend(); 
+        plt.xlabel('time (minutes)')
+        plt.ylabel('concentartion (M)')
+        plt.savefig('plot.resid.%spng' % tuple_repr(tupl))
+        plt.show()
+    return df
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("-r", "--residuals", action="store_true")
+    parser.add_argument("files", nargs="+")
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = get_args()
+    prots = ['S', 'K', 'L', 'V']
+    media = ['elastin', 'gelatin']
+
+    length = 120
+    titlefmt    = 'cat%s on %s'
+    filenamefmt = 'data.%scsv'
+    #files = ['data.K.S.elastin.csv']
+    for fname in args.files:
+        df = process_file(fname)
