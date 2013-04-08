149a217393786a888480fb6cdc6e71500c5493a8
James Fairbanks <james.fairbanks@gatech.edu>
2013-04-08 19:04:05 -0400

    making the code a bit more general

--- ode_learner/ode_sampler.py
+++ ode_learner/ode_sampler.py
@@ -31,18 +31,37 @@ def get_mat(arg1):
            [  0,  0,  1,  0,  0,],
         ])
     return M
+
+def orders_of_magnitue(arg1):
+    """@todo: Docstring for orders_of_magnitue
+
+    :arg1: @todo
+    :returns: @todo
+
+    """
+    P = np.array([
+           [ -1,  1,  0,  0,  0,],
+           [ -1,  1,  1,  0,  0,],
+           [  1, -1, -1,  0,  0,],
+           [  0,  0,  0,  1,  0,],
+           [  0,  0,  1,  0,  0,],
+        ])
+    return 10**P
+
 if __name__ == '__main__':
     d = 5
-    T = 25
+    NUM_T = 25
+    MAX_T = 1.0
     #M = np.matrix(np.zeros((d,d)))
     #M[0,1] = -.5
     #M[1,0] = .5
-    M = get_mat(None) * rand.random((d,d))
+    M = get_mat(None)# * rand.random((d,d))
     vprint('M',M)
-    xseq = np.linspace(0,10,T+1)
-    yseq = np.ones((T+1,d))
-    for i in range(T):
-        yseq[i+1] = M.T.dot(yseq[i]) + yseq[i]
+    xseq = np.linspace(0,MAX_T,NUM_T+1)
+    delta_t = MAX_T/(NUM_T+1)
+    yseq = np.ones((NUM_T+1,d))
+    for i in range(NUM_T):
+        yseq[i+1] = delta_t*M.T.dot(yseq[i]) + yseq[i]
         vprint('y',yseq[i+1])
     df = pd.DataFrame(yseq, index=xseq)
     df.plot()
